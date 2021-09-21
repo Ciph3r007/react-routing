b0f669004ab51ea34749cffe71e88f9c1df61564
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-21 17:43:33 +0600

    Fix package dependencies, add starter routing template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -8,14 +9,18 @@ import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="content">
+        <Route path="/products" component={Products} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/admin" component={Dashboard} />
+        <Route path="/" component={Home} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
